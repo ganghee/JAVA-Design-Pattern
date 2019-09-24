729b50c834708de5cc075ca0921c5a7fde1c1933
gangMac <idiosauri@naver.com>
2019-09-24 21:48:01 +0900

    strategy pattern

---
+++ Strategy Pattern/src/app/Main.java
@@ -0,0 +1,15 @@
+package app;
+
+public class Main{
+    public static void main(String[] args){
+
+        GameCharacter character = new GameCharacter();
+        character.attack();
+
+        character.setWeapon(new Knife());
+        character.attack();
+
+        character.setWeapon(new Sword());
+        character.attack();
+    }
+}
---
+++ Strategy Pattern/src/app/Sword.java
@@ -0,0 +1,8 @@
+package app;
+
+public class Sword implements Weapon{
+    @Override
+    public void attack(){
+        System.out.println("검 공격");
+    }
+}
