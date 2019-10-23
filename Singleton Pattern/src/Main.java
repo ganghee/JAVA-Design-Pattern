f49d7ba0644465a4d74cd856fad3e0777985c506
ganghee <idiosauri@naver.com>
2019-10-23 18:23:54 +0900

    하나의 인스턴스를 생성하여 그 인스턴스의 변수를 공유한다.

---
+++ Singleton Pattern/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main{
+	public static void main(String[] args) {
+		SystemSpeaker speaker1 = SystemSpeaker.getInstance();
+		SystemSpeaker speaker2 = SystemSpeaker.getInstance();
+		
+		System.out.println(speaker1.getVolume());
+		System.out.println(speaker1.getVolume());
+		
+		speaker1.setVolume(11);
+		System.out.println(speaker1.getVolume());
+		System.out.println(speaker1.getVolume());
+		
+		speaker2.setVolume(22);
+		System.out.println(speaker1.getVolume());
+		System.out.println(speaker1.getVolume());
+	}
+}
